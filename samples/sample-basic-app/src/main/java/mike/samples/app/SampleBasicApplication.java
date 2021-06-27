103d687a709e744b98866e821c8a100f6722b2b3
Mike <mllopis11@gmail.com>
2021-06-27 11:51:53 +0200

    Samples: add basic application

---
+++ samples/sample-basic-app/src/main/java/mike/samples/app/SampleBasicApplication.java
@@ -0,0 +1,16 @@
+package mike.samples.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import mike.bootstrap.springboot.application.Application;
+
+@SpringBootApplication
+@ComponentScan(basePackages = { Application.BOOT_BASE_PACKAGE, "mike.samples.app"})
+public class SampleBasicApplication {
+
+    public static void main(String[] args) {
+        Application.standalone(SampleBasicApplication.class, args);
+    }
+
+}
