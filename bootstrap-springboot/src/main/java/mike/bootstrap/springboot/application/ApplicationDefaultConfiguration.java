032a5a1f2e73e103f9242825f625b116c15b725a
Mike <mllopis11@gmail.com>
2021-06-27 11:50:56 +0200

    FIX: Configuration only for Webapp

--- bootstrap-springboot/src/main/java/mike/bootstrap/springboot/application/ApplicationDefaultConfiguration.java
+++ bootstrap-springboot/src/main/java/mike/bootstrap/springboot/application/ApplicationDefaultConfiguration.java
@@ -4,12 +4,14 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @Configuration
+@ConditionalOnClass(LocaleResolver.class)
 class ApplicationDefaultConfiguration {
 	
 	private static final Logger log = LoggerFactory.getLogger(ApplicationDefaultConfiguration.class);
