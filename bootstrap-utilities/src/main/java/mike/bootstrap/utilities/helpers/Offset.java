c6c880abcad8b76f0bf83c705928b8530ce3566b
Mike <mllopis11@gmail.com>
2022-06-11 16:38:52 +0200

    Helpers: add Offset class

---
+++ bootstrap-utilities/src/main/java/mike/bootstrap/utilities/helpers/Offset.java
@@ -0,0 +1,86 @@
+package mike.bootstrap.utilities.helpers;
+
+/**
+ * String offset with position (starts at 1) and length.
+ * 
+ * @author Mike
+ */
+public class Offset {
+
+    private final int position;
+    private final int length;
+    private final int startAt;
+    private final int endAt;
+    
+    /**
+     * @return offset with position and length set to 1
+     */
+    public static Offset of() {
+	return new Offset(1, 1);
+    }
+    
+    /**
+     * @param position offset starts at position
+     * @return offset starting at the given position and minimal length 1.
+     */
+    public static Offset of(int position) {
+	return Offset.of(position, 1);
+    }
+    
+    /**
+     * @param position offset starts at position
+     * @param length   offset length
+     * @return
+     */
+    public static Offset of(int position, int length) {
+	return new Offset(position, length);
+    }
+    
+    /**
+     * Constructor.
+     */
+    private Offset(int position, int length) {
+	PreConditions.test(position > 0, "Offset: position must be a positive number");
+	PreConditions.test(length > 0, "Offset: length must be a positive number");
+	
+	this.position = position;
+	this.length = length;
+	this.startAt = position - 1;
+	this.endAt =this.startAt + this.length;
+    }
+
+    public int position() {
+        return position;
+    }
+
+    public int length() {
+        return length;
+    }
+    
+    /**
+     * Offset starting position (i.e.: position - 1)
+     * 
+     * @return starting position
+     */
+    public int startAt() {
+	return startAt;
+    }
+    
+    /**
+     * Offset ending position (i.e.: position - 1 + length)
+     * <pre>
+     * Offset(2:2): "ABBCC" => "BB" (startAt=1, endAt=4 (excluded))  
+     * </pre>
+     * 
+     * @return ending position 
+     */
+    public int endAt() {
+	return endAt; 
+    }
+
+    @Override
+    public String toString() {
+	return String.format("Offset [pos=%d, len=%d, startAt=%d, endAt=%d]", 
+		position, length, startAt, endAt);
+    }
+}
---
+++ bootstrap-utilities/src/test/java/mike/bootstrap/test/utilities/helpers/OffsetTest.java
@@ -0,0 +1,48 @@
+package mike.bootstrap.test.utilities.helpers;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import mike.bootstrap.utilities.helpers.Offset;
+
+@DisplayName("Helpers::Offset")
+class OffsetTest {
+
+    @ParameterizedTest
+    @CsvSource({"0, 0", "3, 0", "0, 3"})
+    void should_throw_IllegalArgumentException_when_position_or_length_less_than_Zero(int pos, int len) {
+	assertThatIllegalArgumentException()
+		.isThrownBy(() -> Offset.of(pos, len));
+    }
+    
+    @Test
+    void should_return_invalid_offset_when_position_and_length_greater_than_zero() {
+	
+	assertThat(Offset.of(5, 3)).satisfies(offset -> {
+	    assertThat(offset.position()).isEqualTo(5);
+	    assertThat(offset.length()).isEqualTo(3);
+	    assertThat(offset.startAt()).isEqualTo(4);
+	    assertThat(offset.endAt()).isEqualTo(7);
+	    assertThat(offset.toString()).startsWith("Offset [pos=").endsWith("]");
+	});
+	
+	assertThat(Offset.of()).satisfies(offset -> {
+	    assertThat(offset.position()).isEqualTo(1);
+	    assertThat(offset.length()).isEqualTo(1);
+	    assertThat(offset.startAt()).isZero();
+	    assertThat(offset.endAt()).isEqualTo(1);
+	});
+	
+	assertThat(Offset.of(5)).satisfies(offset -> {
+	    assertThat(offset.position()).isEqualTo(5);
+	    assertThat(offset.length()).isEqualTo(1);
+	    assertThat(offset.startAt()).isEqualTo(4);
+	    assertThat(offset.endAt()).isEqualTo(5);
+	});
+    }
+}
